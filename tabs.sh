9474af510ce5b5d9343bad7db8690ef881609171
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2023-08-04 14:01:08 -0700

    Allow specifying how many spaces a tab should be. Default is 4.

--- tabs.sh
+++ tabs.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-VIMRC="/home/$USER/.vimrc"
+VIMRC="/home/${USER}/.vimrc"
 
 tabs() {
-    printf ":set number\n"        > "$VIMRC" # show line numbers
-    printf ":set ai\n"           >> "$VIMRC" # auto-indentation
-    printf ":set shiftwidth=4\n" >> "$VIMRC" # indentation level is 4 spaces
-    printf ":set noexpandtab\n"  >> "$VIMRC" # do not expand a tab to use spaces
-    printf ":set tabstop=4\n"    >> "$VIMRC" # tabs occupy 4 spaces
-    printf "\ntab set to an actual tab\n\n"
+    local n=4
+    if [[ "$#" -ge 1 ]]; then
+        n="$1"
+        if [[ ! "$n" =~ ^[0-9]+$ ]]; then
+            echo "Error: tabs(): Invalid number of spaces: ${n}" >&2
+            exit 1
+        fi  
+    fi 
+
+    printf ":set number\n"          >  "${VIMRC}" # show line numbers
+    printf ":set ai\n"              >> "${VIMRC}" # auto-indentation
+    printf ":set shiftwidth=${n}\n" >> "${VIMRC}" # indentation level is 4 spaces
+    printf ":set noexpandtab\n"     >> "${VIMRC}" # do not expand a tab to use spaces
+    printf ":set tabstop=${n}\n"    >> "${VIMRC}" # tabs occupy 4 spaces
+    printf "\ntab set to an actual tab (${n} spaces)\n\n"
 }
 
 spaces() {
-    printf ":set number\n"        > "$VIMRC" # show line numbers
-    printf ":set ai\n"           >> "$VIMRC" # auto-indentation
-    printf ":set shiftwidth=4\n" >> "$VIMRC" # indentation level is 4 spaces
-    printf ":set expandtab\n"    >> "$VIMRC" # expand tabs to use spaces
-    printf ":set tabstop=4\n"    >> "$VIMRC" # tabs occupy 4 spaces
-    printf "\ntab set to four spaces\n\n"
+    local n=4
+    if [[ "$#" -ge 1 ]]; then
+        n="$1"
+        if [[ ! "$n" =~ ^[0-9]+$ ]]; then
+            echo "Error: spaces(): Invalid number of spaces: ${n}" >&2
+            exit 1
+        fi
+    fi
+
+    printf ":set number\n"       >  "${VIMRC}" # show line numbers
+    printf ":set ai\n"           >> "${VIMRC}" # auto-indentation
+    printf ":set shiftwidth=4\n" >> "${VIMRC}" # indentation level is n spaces
+    printf ":set expandtab\n"    >> "${VIMRC}" # expand tabs to use spaces
+    printf ":set tabstop=${n}\n" >> "${VIMRC}" # tabs occupy n spaces
+    printf "\ntab set to ${n} spaces\n\n"
 }
 
 vmake() {
